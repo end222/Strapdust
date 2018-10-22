8d48b3f10c9b64cdc1f1db808761daeb708f7576
Pablo <pabloorduna98@gmail.com>
2018-10-23 00:37:02 +0200

    Añadir cambios en java

--- src/EjemploCargaDatos.java
+++ src/EjemploCargaDatos.java
@@ -4,7 +4,6 @@ import java.util.Properties;
 import jdbc.Cursor;
 import jdbc.JDBCTemplate;
 import jdbc.MySQLConfiguration;
-import jdbc.OracleConfiguration;
 
 /**
  * Esta clase lee datos de la tabla "title" en MySQL, los procesa y 
@@ -13,43 +12,34 @@ import jdbc.OracleConfiguration;
 public class EjemploCargaDatos {
 
 	public static void main(String args[]) {
-		JDBCTemplate oracle = null;
 		JDBCTemplate mysql = null;
 		Properties prop = new Properties();
 		try {
-			prop.load(EjemploCargaDatos.class
-					.getResourceAsStream("sistemas.properties"));
+			prop.load(EjemploCargaDatos.class.getResourceAsStream("sistemas.properties"));
 			mysql = configureMySQL(prop);
 			
 			// Esta llamada solo se tendria que hacer 
 			// si no existe la tabla de peliculas en Oracle
-			crearTablaDePeliculas(oracle);
+			//crearTablaDePeliculas(oracle);
 			
 			// Esta llamada solo se tendria que hacer
 			// si nos interesa borrar todo el contenido
 			// de la tabla de peliculas en Oracle (por ejemplo
 			// queremos volver a cargar todos los datos)
-			borrarContenidoTablaDePeliculas(oracle);
+			//borrarContenidoTablaDePeliculas(oracle);
 			
 			// Seleccionamos los titulos y el ano de produccion
 			// de las peliculas almacenadas en la tabla
 			// "title" en la BD MySQL.
-			for(Cursor c: mysql.executeQueryAndGetCursor("SELECT title, production_year FROM title")) {
+			for(Cursor c: mysql.executeQueryAndGetCursor("SELECT * FROM sisinfo.administrador")) {
 
 				// De cada fila extraemos los datos y los procesamos. 
 				// A continuacion los insertamos en la BD Oracle.
-				System.out.println("Insertando "+c.getString("title")+" - "+(2013-c.getInteger("production_year")));
-				oracle.executeSentence("INSERT INTO PELICULAS_EJEMPLO(NOMBRE,ANTIGUEDAD) VALUES (?,?)", 
-						c.getString("title"), 2013-c.getInteger("production_year"));
+				System.out.println(c.getString("PDI") + " " + c.getString("NOMBRE") + " " + c.getString("PASS"));
 			}
-			
-			// Finalmente listamos el contenido resultante
-			oracle.executeQuery("SELECT * FROM PELICULAS_EJEMPLO");
-			
 		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
 		} finally {
-			if (oracle != null) oracle.disconnect();
 			if (mysql != null) mysql.disconnect();
 		}
 	}
@@ -67,35 +57,5 @@ public class EjemploCargaDatos {
 		mysql.connect();
 		System.out.println("Conectado a " + mysql);
 		return mysql;
-	}
-
-	private static JDBCTemplate configureOracle(Properties prop)
-			throws InstantiationException, IllegalAccessException,
-			ClassNotFoundException, SQLException {
-		JDBCTemplate oracle;
-		oracle = new JDBCTemplate(new OracleConfiguration(prop.getProperty("database.oracle.host"),
-				prop.getProperty("database.oracle.port"),
-				prop.getProperty("database.oracle.sid")),
-				prop.getProperty("database.oracle.user"),
-				prop.getProperty("database.oracle.password")
-				);
-		oracle.connect();
-		System.out.println("Conectado a " + oracle);
-		return oracle;
-	}
-
-	private static void borrarContenidoTablaDePeliculas(JDBCTemplate o) {
-		StringBuffer sb = new StringBuffer();
-		sb.append("TRUNCATE TABLE PELICULAS_EJEMPLO");
-		o.executeSentence(sb.toString());
-	}
-
-	private static void crearTablaDePeliculas(JDBCTemplate o) {
-		StringBuffer sb = new StringBuffer();
-		sb.append("CREATE TABLE PELICULAS_EJEMPLO(");
-		sb.append("NOMBRE VARCHAR(30) PRIMARY KEY,");
-		sb.append("ANTIGUEDAD NUMBER(4)");
-		sb.append(")");
-		o.executeSentence(sb.toString());
 	}
 }
